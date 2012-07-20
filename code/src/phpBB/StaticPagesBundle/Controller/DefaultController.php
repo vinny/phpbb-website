25cad085fd959aa82c07135bf71d9b8a9ca2ebd7
Michael Cullum <unknownbliss@phpbbdevelopers.net>
2012-07-20 10:56:13 +0100

    Basic static pages bundle implementation

--- code/src/phpBB/StaticPagesBundle/Controller/DefaultController.php
+++ code/src/phpBB/StaticPagesBundle/Controller/DefaultController.php
@@ -6,8 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
-    {
-        return $this->render('phpBBStaticPagesBundle:Default:index.html.twig', array('name' => $name));
-    }
+	public function defaultAction($slug)
+	{
+		if ($container->getParameter('phpbb_static_pages.template.location') 
+		{
+			$static_templates_location = $container->getParameter('phpbb_static_pages.template.location');
+		}
+		else
+		{
+			$static_templates_location = ':Static:'
+		}
+
+		$template_slug = str_replace('/', '.', $slug);
+		$template_location = $static_templates_location . $template_slug . '.html.twig';
+		$static_variables = array(
+			'slug'	=> $slug,
+		);
+
+		if(!$this->engine->exists($template_location))
+		{
+			throw $this->createNotFoundException('This page could not be found');
+		}
+
+		return $this->render($template_location, $static_variables);
+	}
 }
