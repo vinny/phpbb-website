3e5dbd46a2533f7901b2be4f0ba6739716496352
MichaelC <m@michaelcullum.com>
2013-07-13 11:38:10 -0400

    Add merge script

---
+++ merge.sh
@@ -0,0 +1,16 @@
+git fetch public
+git fetch private
+git checkout master
+git merge public/master
+git merge private/master
+git checkout develop
+git merge public/develop
+git merge private/develop
+git merge master
+git checkout private
+git merge private/private
+git merge master
+git checkout sandbox
+git merge private/sandbox
+git merge develop
+git merge private
