3a564bed5d780a0ab460801d55ddaa1b3792fb50
MichaelC <m@michaelcullum.com>
2013-08-13 23:24:57 +0100

    Add private website bundle to private branches

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             //new phpBB\phpBBSessionsAuthBundle\phpBBphpBBSessionsAuthBundle(),
-            //new phpBB\PrivateWebsiteBundle\phpBBPrivateWebsiteBundle(),
+            new phpBB\PrivateWebsiteBundle\phpBBPrivateWebsiteBundle(),
             new phpBB\WebsiteInterfaceBundle\phpBBWebsiteInterfaceBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
---
+++ src/phpBB/PrivateWebsiteBundle/phpBBPrivateWebsiteBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 
+ * @package phpBBPrivateWebsiteBundle
+ * @copyright (c) 2012 phpBB Group
+ * @license Not for re-distribution
+ * @author Unknown Bliss
+ * 
+ */
+
+namespace phpBB\PrivateWebsiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class phpBBPrivateWebsiteBundle extends Bundle
+{
+}
