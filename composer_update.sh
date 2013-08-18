7b7c9b562d9e3d387ae13d33544564b170623b2c
MichaelC <m@michaelcullum.com>
2013-08-18 19:26:07 +0100

    Set -x

--- composer_update.sh
+++ composer_update.sh
@@ -1,17 +1,14 @@
-echo "Git Status:"
+set -x
 git status -sb
 echo ""
-echo "composer.phar Update:"
 php composer.phar self-update
 # Comment this line if no phar in path
 composer self-update
 echo ""
-echo "Deps update:"
-# If no composer in your path then comment this line and uncomment Ln 12
+# If no composer in your path then comment this line and uncomment Ln 10
 composer update -v --dev
 # php composer.phar update --dev
 echo ""
-echo "Tests:"
 bin/phpunit -c app/
 echo ""
 echo "COMPLETE!"
