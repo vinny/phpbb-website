68ad8ca4ea10e7491871705b3687fb0556a1035e
MichaelC <m@michaelcullum.com>
2013-09-22 09:52:30 +0100

    Revert "Scrutinizer fixes". They are rubbish
    This reverts commit 06004605da51547de73a5c5ff5c9b49f1d681541.

--- src/phpBB/WebsiteInterfaceBundle/Tests/Controller/AboutControllerTest.php
+++ src/phpBB/WebsiteInterfaceBundle/Tests/Controller/AboutControllerTest.php
@@ -14,24 +14,24 @@ use phpBB\WebsiteInterfaceBundle\Tests\Controller;
 
 class AboutControllerTest extends BootstrapTestSuite
 {
-    public function testAboutMain()
-    {
-        $client = static::createClient();
-        $this->setClient($client);
-        $client->enableProfiler();
-        $crawler = $client->request('GET', '/about/');
-        $response = $client->getResponse();
-        $this->setupBootstrapping($client, $crawler, $response);
+	public function testAboutMain()
+	{
+		$client = static::createClient();
+		$this->setClient($client);
+		$client->enableProfiler();
+		$crawler = $client->request('GET', '/about/');
+		$response = $client->getResponse();
+		$this->setupBootstrapping($client, $crawler, $response);
 
-        // Title Check
-        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'About phpBB') !== false, 'Title contains About phpBB');
+		// Title Check
+		$this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'About phpBB') !== false, 'Title contains About phpBB');
 
-        // Content Check
-        $this->assertTrue($crawler->filter('html:contains("Millions of people use phpBB on a daily basis, making it the most widely used open source bulletin board system in the world.")')->count() > 0, 'About Home Content Check');
-        $this->assertTrue($crawler->filter('a:contains("The History")')->count() > 0, 'About Home Sidebar Check');
-        $this->assertTrue($crawler->filter('html:contains("Project External Links")')->count() > 0, 'About Home Sidebar Check 2');
+		// Content Check
+		$this->assertTrue($crawler->filter('html:contains("Millions of people use phpBB on a daily basis, making it the most widely used open source bulletin board system in the world.")')->count() > 0, 'About Home Content Check');
+		$this->assertTrue($crawler->filter('a:contains("The History")')->count() > 0, 'About Home Sidebar Check');
+		$this->assertTrue($crawler->filter('html:contains("Project External Links")')->count() > 0, 'About Home Sidebar Check 2');
 
-        // Standard All Page Checks
-        $this->globalTests();
-    }
+		// Standard All Page Checks
+		$this->globalTests();
+	}
 }
