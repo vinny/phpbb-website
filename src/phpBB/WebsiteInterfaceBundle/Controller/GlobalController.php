115acb04d9f1277a8db3b9413fcdaf8fd7556c5a
MichaelC <m@michaelcullum.com>
2013-09-24 21:55:57 +0100

    Actually, we don't need to bother checking for the file as json_decode(false) will result to the same as an invalid result.

--- src/phpBB/WebsiteInterfaceBundle/Controller/GlobalController.php
+++ src/phpBB/WebsiteInterfaceBundle/Controller/GlobalController.php
@@ -69,8 +69,9 @@ class GlobalController extends Controller
 
 		// Get announcements file
 		$blogFile = file_get_contents('https://www.phpbb.com/website/wp_announcements.php?password=thisisnotverysecretbutitdoesntreallyneedtobe');
+		$blogJson = json_decode($blogFile, true);
 
-		$blogAnnouncements = $blogFile === false ? array() : ((json_decode($blogFile, true) === null) ? array() : json_decode($blogFile, true));
+		$blogAnnouncements = $blogJson === null ? array() : $blogJson;
 
 		krsort($blogAnnouncements);
 
