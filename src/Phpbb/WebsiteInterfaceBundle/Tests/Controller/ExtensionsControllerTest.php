08f2e6edd46ad1d7eb51e9d259f9d1936a6ba0da
MichaelC <m@michaelcullum.com>
2014-07-13 17:59:30 +0100

    Add tests

--- src/Phpbb/WebsiteInterfaceBundle/Tests/Controller/ExtensionsControllerTest.php
+++ src/Phpbb/WebsiteInterfaceBundle/Tests/Controller/ExtensionsControllerTest.php
@@ -24,7 +24,119 @@ class ExtensionsControllerTest extends BootstrapTestSuite
 
         // Content Check
         $this->assertTrue($crawler->filter('html:contains("Extensions are the natural successor to modifications")')->count() > 0, 'Extensions Home Content Check');
-        $this->assertTrue($crawler->filter('html:contains("Extensions Rules and Guidelines")')->count() > 0, 'Extensions Sidebar Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
+
+        // Standard All Page Checks
+        $this->globalTests();
+    }
+
+    public function testExtensionsRules()
+    {
+        $objs = $this->setupTest('/extensions/rules-and-guidelines/');
+        $crawler = $objs['crawler'];
+
+        // Title Check
+        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'Rules and Guidelines') !== false, 'Title contains Extensions Rules and Guidelines');
+
+        // Content Check
+        $this->assertTrue($crawler->filter('html:contains("Licensing your Extension")')->count() > 0, 'Extensions Home Content Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
+
+        // Standard All Page Checks
+        $this->globalTests();
+    }
+
+    public function testExtensionsRecDevs()
+    {
+        $objs = $this->setupTest('/extensions/recognised-developer/');
+        $crawler = $objs['crawler'];
+
+        // Title Check
+        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'Recognised Extension Developer') !== false, 'Title contains Recognised Extension Developer');
+
+        // Content Check
+        $this->assertTrue($crawler->filter('html:contains("Do you write great code?")')->count() > 0, 'Extensions Home Content Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
+
+        // Standard All Page Checks
+        $this->globalTests();
+    }
+
+    public function testExtensionsOfficialExts()
+    {
+        $objs = $this->setupTest('/extensions/official-extensions/');
+        $crawler = $objs['crawler'];
+
+        // Title Check
+        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'Official Extensions') !== false, 'Title contains Official Extensions');
+
+        // Content Check
+        $this->assertTrue($crawler->filter('html:contains("These are extensions created and maintained by the")')->count() > 0, 'Extensions Home Content Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
+
+        // Standard All Page Checks
+        $this->globalTests();
+    }
+
+    public function testExtensionsOfficialExtsTeam()
+    {
+        $objs = $this->setupTest('/extensions/official-extensions/team/');
+        $crawler = $objs['crawler'];
+
+        // Title Check
+        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'Extensions Development Team') !== false, 'Title contains Extensions Development Team');
+
+        // Content Check
+        $this->assertTrue($crawler->filter('html:contains("The Extensions Development Team are extension Authors that work under the mentorship of the Extensions Team to create")')->count() > 0, 'Extensions Home Content Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
+
+        // Standard All Page Checks
+        $this->globalTests();
+    }
+
+    public function testExtensionsInstalling()
+    {
+        $objs = $this->setupTest('/extensions/installing/');
+        $crawler = $objs['crawler'];
+
+        // Title Check
+        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'Installing Extensions') !== false, 'Title contains Installing Extensions');
+
+        // Content Check
+        $this->assertTrue($crawler->filter('html:contains("Installing an Extension")')->count() > 0, 'Extensions Home Content Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
+
+        // Standard All Page Checks
+        $this->globalTests();
+    }
+
+    public function testExtensionsWriting()
+    {
+        $objs = $this->setupTest('/extensions/writing/');
+        $crawler = $objs['crawler'];
+
+        // Title Check
+        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'Writing Extensions') !== false, 'Title contains Writing Extensions');
+
+        // Content Check
+        $this->assertTrue($crawler->filter('html:contains("How to write an extension")')->count() > 0, 'Extensions Home Content Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
+
+        // Standard All Page Checks
+        $this->globalTests();
+    }
+
+    public function testExtensionsTeamOverViews()
+    {
+        $objs = $this->setupTest('/extensions/team-overview/');
+        $crawler = $objs['crawler'];
+
+        // Title Check
+        $this->assertTrue(strpos(($crawler->filter('title')->first()->text()), 'Extensions Team Overview') !== false, 'Title contains Extensions Team Overview');
+
+        // Content Check
+        $this->assertTrue($crawler->filter('html:contains("The Extensions Team oversees all activities on phpBB.com with relation to extensions")')->count() > 0, 'Extensions Home Content Check');
+        $this->assertTrue($crawler->filter('html:contains("Extensions Team")')->count() > 0, 'Extensions Sidebar Check');
 
         // Standard All Page Checks
         $this->globalTests();
