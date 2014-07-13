5f13827db15e846bd5deec6ad5bd296eca781d2a
MichaelC <m@michaelcullum.com>
2014-07-13 15:35:37 +0100

    Tiny changes to scripts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-# Ensure you have setup your parameters.yml and have no passkey on your ssh key before running this script.
+echo "Ensure you have setup your parameters.yml and have no passkey on your ssh
+key before running this script. If not hit Ctrl + C now."
+sleep 5
 set -ex
 
 php composer.phar self-update
