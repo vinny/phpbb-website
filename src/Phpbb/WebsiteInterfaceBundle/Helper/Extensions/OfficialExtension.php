0b3be8438c81929a5abb4058d2a6639d69a40a67
MichaelC <m@michaelcullum.com>
2014-07-13 16:44:47 +0100

    Fixes to extension pages PR

--- src/Phpbb/WebsiteInterfaceBundle/Helper/Extensions/OfficialExtension.php
+++ src/Phpbb/WebsiteInterfaceBundle/Helper/Extensions/OfficialExtension.php
@@ -1,9 +1,11 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: battye
- * Date: 28/06/2014
- * Time: 4:47 PM
+ *
+ * @package PhpbbWebsiteInterfaceBundle
+ * @copyright (c) 2014 phpBB Group
+ * @license http://opensource.org/licenses/gpl-3.0.php GNU General Public License v3
+ * @author battye
+ *
  */
 
 namespace Phpbb\WebsiteInterfaceBundle\Helper\Extensions;
@@ -11,12 +13,50 @@ namespace Phpbb\WebsiteInterfaceBundle\Helper\Extensions;
 // A simple helper class to help us iterate through officially sanctioned extensions
 class OfficialExtension
 {
+    /**
+     * Extension Name
+     * @var string
+     * @access protected
+     */
     protected $name;
+
+    /**
+     * Extension Description
+     * @var string
+     * @access protected
+     */
     protected $description;
+
+    /**
+     * Extra Information for Developers
+     * @var string
+     * @access protected
+     */
     protected $descriptionForDevelopers;
+
+    /**
+     * Link to extension cdb page
+     * @var string
+     * @access protected
+     */
     protected $contribution;
+
+    /**
+     * Link to extension github repository
+     * @var string
+     * @access protected
+     */
     protected $github;
 
+    /**
+     * Constructor
+     *
+     * @param string $name                     Extension Name
+     * @param string $description              Extension Description
+     * @param string $descriptionForDevelopers Extra Information for Developers
+     * @param string $contribution             Link to extension cdb page
+     * @param string $github                   Link to extension github repository
+     */
     public function __construct($name, $description, $descriptionForDevelopers, $contribution, $github)
     {
         $this->name = $name;
@@ -26,33 +66,53 @@ class OfficialExtension
         $this->github = $github;
     }
 
-    // Name of the extension
+    /**
+     * Get extension name
+     *
+     * @return string $this->name
+     */
     public function getName()
     {
         return $this->name;
     }
 
-    // Description of the extension
+    /**
+     * Get extension description
+     *
+     * @return string $this->description
+     */
     public function getDescription()
     {
         return $this->description;
     }
 
-    // Extra information for developers
+    /**
+     * Get extra information for developers
+     *
+     * @return string $this->descriptionForDevelopers
+     */
     public function getDescriptionForDevelopers()
     {
         return $this->descriptionForDevelopers;
     }
 
-    // Link to the contribution page
+    /**
+     * Get link to extension's cdb page
+     *
+     * @return string $this->contribution
+     */
     public function getContribution()
     {
         return $this->contribution;
     }
 
-    // Link to the GitHub repository
+    /**
+     * Get link to extension's github repository
+     *
+     * @return string $this->github
+     */
     public function getGithub()
     {
         return $this->github;
     }
-} 
+}
