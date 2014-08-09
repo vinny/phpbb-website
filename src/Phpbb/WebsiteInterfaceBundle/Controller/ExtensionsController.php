6769b68df9c2923a1cf8bdcc85a05218b5f145b4
Matt Friedman <maf675@gmail.com>
2014-08-09 13:38:49 +0200

    Comment out unused checklist link

--- src/Phpbb/WebsiteInterfaceBundle/Controller/ExtensionsController.php
+++ src/Phpbb/WebsiteInterfaceBundle/Controller/ExtensionsController.php
@@ -128,7 +128,7 @@ class ExtensionsController extends Controller
 				array('name' => 'Repack Policy', 'description' => 'How we handle repacking of Extensions.', 'link' => '/extensions/rules-and-guidelines/repack'),
 				array('name' => 'Insta Deny Policy', 'description' => 'How we handle Insta Denies of Extensions.', 'link' => '/extensions/rules-and-guidelines/insta-deny'),
 				array('name' => 'General', 'description' => 'Some general policies on how we handle validation.', 'link' => '/extensions/rules-and-guidelines/general'),
-				array('name' => 'Validation checklist', 'description' => 'A reference for Extensions Authors to improve the validation process for their Extensions.', 'link' => '/extensions/rules-and-guidelines/checklist'),
+				//array('name' => 'Validation checklist', 'description' => 'A reference for Extensions Authors to improve the validation process for their Extensions.', 'link' => '/extensions/rules-and-guidelines/checklist'),
 			)
 		);
 
