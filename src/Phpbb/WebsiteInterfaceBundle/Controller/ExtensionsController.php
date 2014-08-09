18ef255981e804aa5cba63856b04e9ffebf4e19a
Matt Friedman <maf675@gmail.com>
2014-08-09 17:31:21 +0200

    Insta Deny fixes in the extension controller

--- src/Phpbb/WebsiteInterfaceBundle/Controller/ExtensionsController.php
+++ src/Phpbb/WebsiteInterfaceBundle/Controller/ExtensionsController.php
@@ -129,7 +129,7 @@ class ExtensionsController extends Controller
 			'header_css_image' => 'mods',
 			'policies'		 => array(
 				array('name' => 'Repack Policy', 'description' => 'How we handle repacking of Extensions.', 'link' => '/extensions/rules-and-guidelines/repack'),
-				array('name' => 'Insta Deny Policy', 'description' => 'How we handle Insta Denies of Extensions.', 'link' => '/extensions/rules-and-guidelines/insta-deny'),
+				array('name' => 'Insta Deny Policy', 'description' => 'How we handle Insta-Denies of Extensions.', 'link' => '/extensions/rules-and-guidelines/insta-deny'),
 				array('name' => 'General', 'description' => 'Some general policies on how we handle validation.', 'link' => '/extensions/rules-and-guidelines/general'),
 				//array('name' => 'Validation checklist', 'description' => 'A reference for Extensions Authors to improve the validation process for their Extensions.', 'link' => '/extensions/rules-and-guidelines/checklist'),
 			)
@@ -155,7 +155,7 @@ class ExtensionsController extends Controller
 			'header_css_image' => 'mods',
 		);
 
-		return $this->render('PhpbbWebsiteInterfaceBundle:Extensions:insta_deny.html.twig', $templateVariables);
+		return $this->render('PhpbbWebsiteInterfaceBundle:Extensions:insta-deny.html.twig', $templateVariables);
 	}
 
 	public function generalAction()
