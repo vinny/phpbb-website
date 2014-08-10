de1f3a1521c929c1a5254d9a286889207573b5d9
Michael C <m@michaelcullum.com>
2014-08-10 18:32:33 +0200

    Try pulling instead

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,14 @@
-set -x
+set -ex
 git status -sb
-
 sleep 5
-
 git fetch public
-
 git fetch private
-
 git checkout private
-
 bin/phpunit -c app/
-
 sleep 10
-
 git checkout www1-stable
-
-git merge private/www1-stable
-
+git pull private www1-stable
 git merge --no-ff private
-
 bin/phpunit -c app/
-
 sleep 10
-
 git push private www1-stable
