8c3cb864322dd646d8a6424b0fd7afe6622b1002
MichaelC <m@michaelcullum.com>
2014-08-16 00:23:21 +0100

    Remove private bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Phpbb\PrivateWebsiteBundle\PhpbbPrivateWebsiteBundle(),
+            //new Phpbb\PrivateWebsiteBundle\PhpbbPrivateWebsiteBundle(),
             new Phpbb\WebsiteInterfaceBundle\PhpbbWebsiteInterfaceBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Phpbb\PhpbbIntergrationBundle\PhpbbIntergrationBundle(),
