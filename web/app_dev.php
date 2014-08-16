f5a9dea6560d8e01305208867bbb267175c2f5ac
Michael C. <m@michaelcullum.com>
2014-08-16 15:48:14 +0100

    Remove my old IP from allowed IPs into app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -15,7 +15,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
-	'94.15.208.141',
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
