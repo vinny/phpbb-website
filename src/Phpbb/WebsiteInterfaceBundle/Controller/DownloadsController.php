b19ff03fb3219f04345ed9e2cf92a8df32473a4a
MichaelC <m@michaelcullum.com>
2014-08-28 11:05:55 +0100

    Add routing and bulk out controller a little
    WEBSITE-1083

--- src/Phpbb/WebsiteInterfaceBundle/Controller/DownloadsController.php
+++ src/Phpbb/WebsiteInterfaceBundle/Controller/DownloadsController.php
@@ -13,12 +13,13 @@ namespace Phpbb\WebsiteInterfaceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Phpbb\WebsiteInterfaceBundle\Utilities\DownloadManager;
 use Symfony\Component\HttpFoundation\Request;
+use Phpbb\WebsiteInterfaceBundle\Entity\Download;
 
 class DownloadsController extends Controller
 {
-	public function downloadsAction(Request $request, $branch = '3.1')
+	public function homeAction(Request $request, $branch = '3.1')
 	{
-		$downloadManager = new DownloadManager;
+		$downloadManager = new DownloadManager();
 		$downloadManager->setBranch($branch);
 
 		$templateVariables = array(
@@ -45,13 +46,20 @@ class DownloadsController extends Controller
 		return $this->render('PhpbbWebsiteInterfaceBundle::downloads.html.twig', $templateVariables);
 	}
 
-	public function downloadHandler(Request $request, $package)
+	public function downloadHandlerAction(Request $request, $package)
 	{
+		$download = new Download();
+		$download->setConfigurableOptions($package, $request->getClientIp());
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($download);
+		$em->flush();
+
+		return $this->redirect(('https://download.phpbb.com/pub/release/' . $package), 301);
 	}
 
-	public function downloadRedirectHandler(Request $request, $branch = 'latest')
+	public function downloadRedirectHandlerAction(Request $request, $branch = 'latest')
 	{
-		$downloadManager = new DownloadManager;
+		$downloadManager = new DownloadManager();
 		$branch = ($branch == 'latest') : '3.1' ? $branch;
 		$downloadManager->setBranch($branch);
 		return $this->downloadHandler($request, $this->downloadManager->getMainPackageName)
