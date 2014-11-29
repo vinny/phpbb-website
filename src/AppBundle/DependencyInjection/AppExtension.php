246289c9df5c86a4e94463fa9eeb75652d66a721
MichaelC <m@michaelcullum.com>
2014-11-29 22:09:42 +0100

    [CS Revisions] Rename DI Extension

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -20,7 +20,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class PhpbbWebsiteInterfaceExtension extends Extension
+class AppExtension extends Extension
 {
     /**
      * {@inheritDoc}
