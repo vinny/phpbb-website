60f837090b9b7710c5228bf4d75e90cf54fad395
Michael C. <m@michaelcullum.com>
2015-01-20 01:51:00 +0000

    Remove this sidebar check test as well

--- src/AppBundle/Tests/Controller/DevelopmentControllerTest.php
+++ src/AppBundle/Tests/Controller/DevelopmentControllerTest.php
@@ -26,7 +26,7 @@ class DevelopmentControllerTest extends BootstrapTestSuite
 		$this->assertTrue($crawler->filter('html:contains("bug tracker, testing infrastructure, a wiki, a discussion board and various other helpful development tools. Visit this site to get involved in phpBB development.")')->count() > 0, 'Development Home Content Check');
 
 		// Sidebar Check
-		$this->assertTrue($crawler->filter('html:contains("To receive information on new releases of phpBB as they become available you are advised to subscribe to our mailing list.")')->count() > 0, 'Development Home Sidebar Check 1');
+		//$this->assertTrue($crawler->filter('html:contains("To receive information on new releases of phpBB as they become available you are advised to subscribe to our mailing list.")')->count() > 0, 'Development Home Sidebar Check 1');
 		$this->assertTrue($crawler->filter('html:contains("Designing prosilver")')->count() > 0, 'Development Home Sidebar Check 2');
 		$this->assertTrue($crawler->filter('html:contains("check out our sponsor")')->count() > 0, 'Development Home Sidebar Check 3');
 		$this->assertTrue($crawler->filter('div.carbonad')->count() == 1, 'Check for Carbon Ads');
