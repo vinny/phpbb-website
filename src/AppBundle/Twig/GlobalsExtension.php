b97043e0045594042288bfc4c1a9dabe3b556997
Marc Alexander <admin@m-a-styles.de>
2017-09-09 13:53:10 +0200

    Update demo to 3.1 and 3.2 and remove 3.0

--- src/AppBundle/Twig/GlobalsExtension.php
+++ src/AppBundle/Twig/GlobalsExtension.php
@@ -643,7 +643,8 @@ class GlobalsExtension extends \Twig_Extension implements \Twig_Extension_Global
 			// phpBB Demo
 			'DEMO_30X'			=> 'http://try-phpbb.com/30x',
 			'demo_31x'			=> 'http://try-phpbb.com/31x',
-			'latest_demo'		=> 'http://try-phpbb.com/31x',
+			'demo_32x'			=> 'http://try-phpbb.com/32x',
+			'latest_demo'		=> 'http://try-phpbb.com/32x',
 
 			'MANAGEMENT_TEAM'	=> '/community/memberlist.php?mode=group&g=13330',
 		);
